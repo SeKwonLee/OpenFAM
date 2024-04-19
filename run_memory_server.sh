0472afae39061a7b69ad8dced373bc303060a909
Sekwon Lee <sekwon.lee@hpe.com>
2024-04-19 11:57:17 -0700

    Upload some new scripts

--- run_memory_server.sh
+++ run_memory_server.sh
@@ -7,9 +7,12 @@ INSTALL_DIR=$BUILD_DIR
 #set library path
 export LD_LIBRARY_PATH=$THIRDPARTY_BUILD/lib/:$THIRDPARTY_BUILD/lib64/:$LD_LIBRARY_PATH
 
+#set environment variable for huge pages
+export RDMAV_HUGEPAGES_SAFE=1
+
 #Build and run test with MemoryServer allocator
 cd $BUILD_DIR
 
 CONFIG_OUT_DIR=$BUILD_DIR/test/config_files/config-cis-rpc-meta-rpc-mem-rpc
 export OPENFAM_ROOT=$CONFIG_OUT_DIR
-bin/memory_server -m 0 > /dev/null 2>&1 &
+numactl --physcpubind=0 --membind=0 bin/memory_server -m 0 > /dev/null 2>&1 &
--- scripts/shm_numa_mount.sh
+++ scripts/shm_numa_mount.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-sudo mount -t tmpfs -o size=64g,mpol=bind:0 tmpfs /dev/shm
+NUMA_NODE=$1
+
+rm -rf /dev/shm/*
+sudo umount /dev/shm
+sudo mount -t tmpfs -o size=64g,mpol=bind:${NUMA_NODE} tmpfs /dev/shm
 sudo chmod -R 777 /dev/shm
