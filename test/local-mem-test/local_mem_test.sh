cc8209736fe8835bd44da14b7f64c5f8ed7354b0
Sekwon Lee <sekwon.lee@hpe.com>
2024-07-03 09:51:49 -0700

    Upload script

---
+++ test/local-mem-test/local_mem_test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Usage: ./test <total memory size (MB)> <access size (Bytes)> <isRand: 0 or 1> <zipf: 0 - Uniform> <num threads> <numa_alloc: 0 or 1>
+
+pattern="zipf-0.99"
+for numThreads in 1 2 4 8
+do
+    for access_size in 64 128 256 512 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576 2097152
+    do
+        ./memcpy_test 8192 ${access_size} 1 0.99 ${numThreads} 0 &>> log-${pattern}.txt
+    done
+    printf "\n\n" &>> log-${pattern}.txt
+done
+
+pattern="uniform"
+for numThreads in 1 2 4 8
+do
+    for access_size in 64 128 256 512 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576 2097152
+    do
+        ./memcpy_test 8192 ${access_size} 1 0 ${numThreads} 0 &>> log-${pattern}.txt
+    done
+    printf "\n\n" &>> log-${pattern}.txt
+done
