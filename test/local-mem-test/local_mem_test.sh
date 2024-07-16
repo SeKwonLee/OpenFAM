428f2fa68c06c5f0fb6f1bef66c0100664d91c56
Sekwon Lee <sekwon.lee@hpe.com>
2024-07-15 19:59:37 -0700

    update

--- test/local-mem-test/local_mem_test.sh
+++ test/local-mem-test/local_mem_test.sh
@@ -7,10 +7,11 @@ for numThreads in 1 2 4 8
 do
     for access_size in 64 128 256 512 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576 2097152
     do
-        ./memcpy_test 8192 ${access_size} 1 0.99 ${numThreads} 0 &>> log-${pattern}.txt
+        sudo ./memcpy_test 8192 ${access_size} 1 0.99 ${numThreads} 0 1 /mnt/hugetlbfs/ &>> log-huge-${pattern}.txt
         #numactl --cpunodebind=0 ./memcpy_test 8192 ${access_size} 1 0.99 ${numThreads} 0 &>> log-${pattern}.txt
+        sudo rm /mnt/hugetlbfs/*
     done
-    printf "\n\n" &>> log-${pattern}.txt
+    printf "\n\n" &>> log-huge-${pattern}.txt
 done
 
 pattern="uniform"
@@ -18,8 +19,9 @@ for numThreads in 1 2 4 8
 do
     for access_size in 64 128 256 512 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576 2097152
     do
-        ./memcpy_test 8192 ${access_size} 1 0 ${numThreads} 0 &>> log-${pattern}.txt
+        sudo ./memcpy_test 8192 ${access_size} 1 0 ${numThreads} 0 1 /mnt/hugetlbfs/ &>> log-huge-${pattern}.txt
         #numactl --cpunodebind=0 ./memcpy_test 8192 ${access_size} 1 0 ${numThreads} 0 &>> log-${pattern}.txt
+        sudo rm /mnt/hugetlbfs/*
     done
-    printf "\n\n" &>> log-${pattern}.txt
+    printf "\n\n" &>> log-huge-${pattern}.txt
 done
