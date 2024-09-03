baaee6743799290de2007e5604e5ff81d86adb5a
Sekwon Lee <sekwon.lee@hpe.com>
2024-09-02 20:48:42 -0500

    Update

--- scripts/config_setup.sh
+++ scripts/config_setup.sh
@@ -15,4 +15,4 @@ module swap PrgEnv-cray/8.3.3 PrgEnv-gnu/8.3.3
 module swap gcc/12.1.0 gcc/11.2.0
 
 #Export all paths
-export OPENFAM_ROOT=/home/leesek/projects/OpenFAM/build
+export OPENFAM_ROOT=/home/leesek/projects/OpenFAM/build/config
